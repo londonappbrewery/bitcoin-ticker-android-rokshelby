63c3e037ac9436043b2b78c18b20ce26cee1618f
rakeem-mac <rokshelby@icloud.com>
2019-06-09 13:15:08 -0500

    done

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,16 +12,21 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
 
     // Member Variables:
     TextView mPriceTextView;
@@ -31,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPriceTextView = (TextView) findViewById(R.id.priceLabel);
-        Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+
+
+        mPriceTextView =  findViewById(R.id.priceLabel);
+        Spinner spinner = findViewById(R.id.currency_spinner);
 
         // Create an ArrayAdapter using the String array and a spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -45,35 +52,72 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+              @Override
+              public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                  Log.d("spina", "Selected: " + parent.getItemAtPosition(position));
+                 // Toast.makeText(getApplicationContext(),"item selected",Toast.LENGTH_SHORT).show();
+                  letsDoSomeNetworking(parent.getItemAtPosition(position).toString());
+              }
+
+              @Override
+              public void onNothingSelected(AdapterView<?> parent) {
+                  Log.d("spina", "nothing selected");
+                 // Toast.makeText(getApplicationContext(),"nothing selected",Toast.LENGTH_SHORT).show();
+              }
+          }
+
+
+        );
+
 
     }
 
     // TODO: complete the letsDoSomeNetworking() method
     private void letsDoSomeNetworking(String url) {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        AsyncHttpClient client = new AsyncHttpClient();
+        String tempString = BASE_URL + url;
+        client.get(tempString, new JsonHttpResponseHandler() {
+            @Override
+            public void onStart()
+            {
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                Log.d("spina", "Request success");
+                Log.d("spina" , "" + response);
+                updateUI(response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, e, errorResponse);
+                Log.d("spina", "Request fail! Status code: " + statusCode);
+                Log.d("spina", "Fail response: " + errorResponse);
+                Log.e("ERROR", e.toString());
+            }
+        });
 
 
     }
 
+    private void updateUI(JSONObject response)
+    {
+        String name;
+        try
+        {
+            name = response.getString("last");
+            mPriceTextView.setText(name);
+            //Toast.makeText(getApplicationContext(),name, Toast.LENGTH_SHORT).show();
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+
+        }
+    }
 
 }
